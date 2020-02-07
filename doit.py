1d0c7f0a3465a8313949521bcc06b710cd9047c8
CheneyJin <cheneyjin@outlook.com>
2020-02-07 13:14:10 +0800

    1

--- doit.py
+++ doit.py
@@ -2,16 +2,18 @@
 """
 @Project: 2019-nCoV
 @Author: CheneyJin
-@Time: 2020/2/6 13:18
+@Time: 2020/2/1 13:18
 """
 
 import json
 import time
+import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
 import math
 import scipy.integrate as spi
 import numpy as np
+from decimal import Decimal
 
 from pylab import mpl
 
@@ -49,7 +51,7 @@ for daily in whole_list:
                                      daily["curedCount"], daily["deadCount"], date]
         whole_date_set.add(date)
         whole_index = whole_index + 1
-
+# , ignore_index=True
 whole_df = whole_df.sort_values("date", ascending=True, inplace=False, ignore_index=True)
 # xticks=tuple(whole_df["date"])
 whole_df.plot()
@@ -102,7 +104,7 @@ for daily in region_list:
 
 
 def cal_r0(confirmed, suspected, t):
-    # defi是确诊人数；susp是疑似人数；t是疾病已爆发时间
+    # confirmed是确诊人数；suspected是疑似人数；t是疾病已爆发时间
 
     # p为疑似病例转化为确诊病例的概率
     p = 0.695
@@ -120,27 +122,36 @@ def cal_r0(confirmed, suspected, t):
     return r0_1, r0_2
 
 
-r01, r02 = cal_r0(28060, 24702, 60)
-
-print("r0:{0}~{1}".format(r01, r02))
+t_start = 23 + 24
+r0_data_frame = pd.DataFrame(columns=("r0", "r1"))
+r0_index = 0
+for index, row in whole_df.iterrows():
+    r01, r02 = cal_r0(row["confirmed"], row["suspected"], t_start)
+    print("R0 -> {0}~{1}".format(Decimal(r01).quantize(Decimal("0.0000")), Decimal(r02).quantize(Decimal("0.0000"))))
+    r0_data_frame.loc[r0_index] = [r01, r02]
+    t_start = t_start + 1
+    r0_index = r0_index + 1
+r0_data_frame.plot(title="RO-MIN-MAX")
+plt.show()
 # print(each_region_data_frame_dict)
 
-# β为疾病传播概率
-beta = 0.2586
 # gamma_1为潜伏期治愈率
 gamma_1 = 0
 # gamma_2为感染者治愈率
-gamma_2 = 0.0235
+gamma_2 = 1188 / 24434  # 0.0235
+
+# β为疾病传播概率
+beta = 3.463173 * (1 / 14)  # 0.2561
 # alpha为潜伏期发展为患者的比例
 alpha = 1
 # die为死亡率
-die = 0.02691
+die = 564 / 24434
 # I_0为感染个体的初始比例
-I_0 = 1e-6
+I_0 = 13 / 12000000
 # E_0为潜伏期个体的初始比例
 E_0 = I_0 * 51
 # R_0为治愈个体的初始比例
-R_0 = 1e-6 / 2
+R_0 = 0
 # S_0为易感个体的初始比例
 S_0 = 1 - I_0 - E_0 - R_0
 # D_0为死亡个体初始比例
@@ -170,7 +181,23 @@ def funcSI(prop, _):
 T_range = np.arange(0, T + 1)
 
 RES = spi.odeint(funcSI, INI, T_range)
+infection = RES[:, 1]
 
+apex = np.where(infection == np.max(infection))[0][0]
+safe_data = np.where(infection < 0.075)[0]
+safe = 0
+# print(safe_data)
+
+for i in range(len(safe_data)):
+    if safe_data[i + 1] - safe_data[i] > 10:
+        safe = safe_data[i + 1]
+        break
+print(safe)
+apex_date = datetime.datetime.strptime("2019-12-08", "%Y-%m-%d") + datetime.timedelta(days=int(apex))
+safe_date = datetime.datetime.strptime("2019-12-08", "%Y-%m-%d") + datetime.timedelta(days=int(safe))
+
+print(apex_date)
+print(safe_date)
 plt.plot(RES[:, 0], color='darkblue', label='Susceptible', marker='.')
 plt.plot(RES[:, 1], color='red', label='Infection', marker='.')
 plt.plot(RES[:, 2], color='orange', label='E', marker='.')
